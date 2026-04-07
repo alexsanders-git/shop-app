acb54bd9fa317dcdf94795acbafe26700ba50036
Alex <alex.sanders@ukr.net>
2026-04-07 21:20:12 +0300

    feat(home): add dynamic product sections and debugbar

--- app/Livewire/HomeComponent.php
+++ app/Livewire/HomeComponent.php
@@ -3,11 +3,27 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Product;
 
 class HomeComponent extends Component
 {
     public function render()
     {
-        return view('livewire.home-component');
+        $hit_products = Product::query()
+            ->orderBy('id', 'desc')
+            ->where('is_hit', '=', 1)
+            ->limit(4)
+            ->get();
+
+        $new_products = Product::query()
+            ->orderBy('id', 'desc')
+            ->where('is_new', '=', 1)
+            ->limit(8)
+            ->get();
+
+        return view( 'livewire.home-component', [
+            'hit_products' => $hit_products,
+            'new_products' => $new_products,
+        ] );
     }
 }
